4f3ba04d9573b54dbed1fa9afa1f15f7483edf11
Raiondesu <a.raiondesu@gmail.com>
2018-06-27 19:20:41 +0300

    Removed abortable for now.

--- src/abortable/index.ts
+++
@@ -1,7 +0,0 @@
-import AbortablePromise from './abortablePromise';
-import abortableFetch from './abortableFetch';
-
-export default Object.freeze({
-  Promise: AbortablePromise,
-  fetch: abortableFetch
-});
--- src/adapter.ts
+++ src/adapter.ts
@@ -1,12 +1,10 @@
-import Abortable from './abortable'
-
 export default abstract class Adapter {
   public abstract request(url: string, config: RequestInit): Promise<any>;
 }
 
 export class DefaultAdapter extends Adapter {
   public request(url: string, config: RequestInit) {
-    return Abortable.fetch(url, config);
+    return fetch(url, config);
   }
 }
 
@@ -15,16 +13,16 @@ export class DefaultAdapter extends Adapter {
 
 export class DefaultJsonAdapter extends Adapter {
   public request(url: string, config: RequestInit) {
-    return new Abortable.Promise(_abort => (resolve, reject) => {
-      Abortable.fetch(url, config).then(resp => resp.json().then(resolve).catch(reject)).catch(reject);
+    return new Promise((resolve, reject) => {
+      fetch(url, config).then(resp => resp.json().then(resolve).catch(reject)).catch(reject);
     });
   }
 }
 
 export class DefaultStreamAdapter extends Adapter {
   public request(url: string, config: RequestInit) {
-    return new Abortable.Promise<ReadableStream | null>(_abort => (resolve, reject) => {
-      Abortable.fetch(url, config).then(resp => resolve(resp.body)).catch(reject);
+    return new Promise<ReadableStream | null>((resolve, reject) => {
+      fetch(url, config).then(resp => resolve(resp.body)).catch(reject);
     });
   }
 }
